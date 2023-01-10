80e1a3b7d716a9601aa4aa248ae3f0ce0b7cb551
wuriyanto <wuriyanto48@yahoo.co.id>
2023-01-10 14:27:34 +0700

    fix variadic params

--- dist/lib/hmac.d.ts
+++ dist/lib/hmac.d.ts
@@ -1,5 +1,5 @@
-export function md5(key: Buffer | KeyObject, ...datas: string | Buffer): string;
-export function sha1(key: Buffer | KeyObject, ...datas: string | Buffer): string;
-export function sha256(key: Buffer | KeyObject, ...datas: string | Buffer): string;
-export function sha384(key: Buffer | KeyObject, ...datas: string | Buffer): string;
-export function sha512(key: Buffer | KeyObject, ...datas: string | Buffer): string;
+export function md5(key: Buffer | KeyObject, ...datas: (string | Buffer)[]): string;
+export function sha1(key: Buffer | KeyObject, ...datas: (string | Buffer)[]): string;
+export function sha256(key: Buffer | KeyObject, ...datas: (string | Buffer)[]): string;
+export function sha384(key: Buffer | KeyObject, ...datas: (string | Buffer)[]): string;
+export function sha512(key: Buffer | KeyObject, ...datas: (string | Buffer)[]): string;
